57048f7c5fdf467477a77af96ab377038f478e73
Whitman Huntley <whitman.colm@gmail.com>
2018-01-23 16:43:00 -0500

    Updated bot to v3.1-Alpha

--- src/commandHandler/module.go
+++ src/commandHandler/module.go
@@ -3,43 +3,49 @@ package nil
 import (
 	f "../foundation"
 	dsg "github.com/bwmarrin/discordgo"
-	"strconv"
+	//	"strconv"
 	"strings"
 )
 
+/* # MessageCreate
+* The world's bigest switch statment
+*
+* This is a very big switch statment run commands. It reads all the messages in
+* all the servers its in, determines which ones are commands, and then sees
+* what in all the commands mean and then takes the appropriate action.
+*
+* Parameters:
+* - s (type *discordgo.Session) | The current running discord session,
+*     (discordgo needs that always apparently)
+* - m (type *discordgo.Message) | The message thats to be acted upon.
+*
+* TODO: See if it can be made so it doesn't have to read every single message
+*       ever.
+*
+* TODO: Break this one function up to smaller functions that only run if a user
+*       has a certain role
+*
+* NOTE: Please delegate what the command actually does to a function. This
+*       method should only be used to determine what the user is acutally
+*       trying to do.
+ */
 func MessageCreate(s *dsg.Session, m *dsg.MessageCreate) ***REMOVED***
-	// This switch statment makes sure the bot runs when its triggered and the user has the perms to trigger it.
-	// Prevents:
-	// - Bot posted something that would trigger itself, possibly creating an infinite loop
-	// - Message posted doesn't have the bot's prefix
-	// - Command was posted in a channel where the bot shouldn't respond to commands
-	// - Bot whitelists channels and the command was run in a channel not on the whitelist.
-	//
-	// Allows:
-	// - Priority "Alpha" users to run commands regardless of any other rules
-	// - Checks if priority "Bravo" users can override, lets command run if so.
-	//
-	// IF THESE CONDITIONS ARE MET THEN NO ERROR WILL BE SENT TO EITHER DISCORD OR LOGGED.
-	// THIS IS BY DESIGN. DON'T CHANGE IT THINKING I WAS JUST LAZY.
-
-	switch true ***REMOVED***
-	case !strings.HasPrefix(m.Content, f.MyBot.Prefs.Prefix):
-		return
-	case f.Contains(f.MyBot.Users.AlphaUsers, m.Author.ID):
-		break
-	case f.MyBot.Users.BravoOverride && f.Contains(f.MyBot.Users.BravoUsers, m.Author.ID):
-		break
-	case !strings.HasPrefix(m.Content, f.MyBot.Prefs.Prefix):
-		return
-	case f.Contains(f.MyBot.Perms.BlacklistedChannels, m.ChannelID):
-		return
-	case f.MyBot.Perms.WhitelistChannels && !f.Contains(f.MyBot.Perms.WhitelistedChannels, m.ChannelID):
+	// The message is checked to see if its a command and can be run
+	canRunCommand, err := canTriggerBot(s, m.Message)
+	if err != nil ***REMOVED***
+		s.ChannelMessageSend(m.ChannelID, "**FATAL. ERROR ENCOUNTERED IN PARSING MESSAGE. DETAILS FOLLOW:**\n"+err.Error()+"\n**CRASHING THE BOT.** Have a good day!")
+		panic(-1)
+	***REMOVED***
+	if canRunCommand != true ***REMOVED***
 		return
 	***REMOVED***
 
+	// Removing case sensitivity:
+	messageSanatized := strings.ToLower(m.Content)
+
 	// The trailing > is cut off the message so the commands can be more easily handled.
-	message := strings.SplitAfterN(m.Content, ">", 2)
-	message = strings.SplitAfterN(message[1], " ", -1)
+	msg := strings.SplitAfterN(messageSanatized, f.MyBot.Prefs.Prefix, 2)
+	message := strings.SplitAfterN(msg[1], " ", -1)
 
 	// Now the message is run to see if its a valid command.
 	switch message[0] ***REMOVED***
@@ -49,21 +55,116 @@ func MessageCreate(s *dsg.Session, m *dsg.MessageCreate) ***REMOVED***
 		s.ChannelMessageSend(m.ChannelID, "Pong!")
 	case "info":
 		s.ChannelMessageSendEmbed(m.ChannelID, getBotInfo())
-	case "perms":
-		s.ChannelMessageSendEmbed(m.ChannelID, getPerms(m.Author.ID, m.Author.AvatarURL("9"), m.Author.Mention()))
+	case "rolelist":
+		run, err := f.HasRole(s, m.Message, "")
+		if err != nil ***REMOVED***
+			errorLong := "Error! Failed to retrieve  Details below:\n```" + err.Error() + "```"
+			s.ChannelMessageSend(m.ChannelID, errorLong)
+			return
+		***REMOVED***
+		if run == false && f.MyBot.Users.ReportPermFails == true ***REMOVED***
+			s.ChannelMessageSend(m.ChannelID, "You do not have permission to use that command.")
+		***REMOVED*** else if run == false && f.MyBot.Users.ReportPermFails == false ***REMOVED***
+			return
+		***REMOVED*** else if run == true ***REMOVED***
+			roles, err := getRoles(m.Message)
+			if err != nil ***REMOVED***
+				errorLong := "Error! Failed to retrieve  Details below:\n```" + err.Error() + "```"
+				s.ChannelMessageSend(m.ChannelID, errorLong)
+				return
+			***REMOVED***
+			s.ChannelMessageSend(m.ChannelID, roles)
+		***REMOVED***
 	default:
 		s.ChannelMessageSend(m.ChannelID, "Sorry, I don't understand.")
 	***REMOVED***
 ***REMOVED***
 
+/* # Check if user can run command
+* This switch statment makes sure the bot runs when its triggered and the user has the perms to trigger it.
+* Prevents:
+* - Bot posted something that would trigger itself, possibly creating an infinite loop
+* - Message posted doesn't have the bot's prefix
+* - Command was posted in a channel where the bot shouldn't respond to commands
+* - Bot whitelists channels and the command was run in a channel not on the whitelist.
+* - Users with a blacklisted role from running the bot
+*
+* NOTE: Users who have "admin" roles (according to the bot's json data)
+*       will have the abilityto run commands regardless of any other rules
+*
+* NOTE: IF THESE CONDITIONS ARE MET THEN NO ERROR WILL BE SENT TO EITHER DISCORD OR LOGGED.
+* THIS IS BY DESIGN. DON'T CHANGE IT THINKING I WAS JUST LAZY.
+ */
+func canTriggerBot(s *dsg.Session, m *dsg.Message) (bool, error) ***REMOVED***
+	admin, err := f.HasRole(s, m, "")
+	if err != nil ***REMOVED***
+		return false, err
+	***REMOVED***
+
+	switch true ***REMOVED***
+	case m.Author.ID == s.State.User.ID:
+		return false, nil
+	case !strings.HasPrefix(m.Content, f.MyBot.Prefs.Prefix):
+		return false, nil
+	case admin:
+		return true, nil
+	case f.Contains(f.MyBot.Perms.BlacklistedChannels, m.ChannelID) == true:
+		return false, nil
+	case f.MyBot.Perms.WhitelistChannels && !f.Contains(f.MyBot.Perms.WhitelistedChannels, m.ChannelID):
+		return false, nil
+	***REMOVED***
+	for _, b := range f.MyBot.Users.BlacklistedRoles ***REMOVED***
+		blacklisted, err := f.HasRole(s, m, b)
+		if err != nil ***REMOVED***
+			return false, err
+		***REMOVED***
+		if blacklisted ***REMOVED***
+			return false, nil
+		***REMOVED***
+	***REMOVED***
+	return true, nil
+***REMOVED***
+
+/* # Get server roles
+* A g-d impossibility.
+*
+* Parameters:
+* - m (type *discordgo.Message) | The message used for data extraction about
+*	the guild and its roles.
+*
+* Returns:
+* - (type string) | A string list of all the roles.
+* - (type error)  | Any errors that may have come up.
+*
+* NOTE: If you print this into a discord chat, it WILL mention @everyone
+ */
+func getRoles(m *dsg.Message) (string, error) ***REMOVED***
+	guild, err := f.GetGuild(f.DG, m)
+	if err != nil ***REMOVED***
+		return "", err
+	***REMOVED***
+	roles, err := f.DG.GuildRoles(guild.ID)
+	if err != nil ***REMOVED***
+		return "", err
+	***REMOVED***
+	role := "Server role list:\n```\n"
+	for _, r := range roles ***REMOVED***
+		role += "Name: " + r.Name + "; ID: " + r.ID + ";\n"
+	***REMOVED***
+	role += "```"
+	return role, nil
+***REMOVED***
+
+// Returns a messageEmbed about the bot; its a function because if it was a
+// variable some of the data doesn't work properly.
 func getBotInfo() *dsg.MessageEmbed ***REMOVED***
 	return &dsg.MessageEmbed***REMOVED***
 		Author:      &dsg.MessageEmbedAuthor***REMOVED******REMOVED***,
 		Color:       0x073642,
-		Title:       "MyBot Information",
+		Title:       "SkilBot Information",
 		Description: "A list of commands can be brought up with `" + f.MyBot.Prefs.Prefix + "help`.",
 		Thumbnail: &dsg.MessageEmbedThumbnail***REMOVED***
-			URL:    "https://pbs.twimg.com/media/CLI0Kd5UcAAkpiM.png",
+			URL:    "https://i.imgur.com/lPTAiFE.png",
 			Width:  64,
 			Height: 64,
 		***REMOVED***,
@@ -76,41 +177,6 @@ func getBotInfo() *dsg.MessageEmbed ***REMOVED***
 			&dsg.MessageEmbedField***REMOVED***
 				Name:   "Github Link",
 				Value:  "https://github.com/skilstak/discord-public",
-				Inline: true,
-			***REMOVED***,
-		***REMOVED***,
-	***REMOVED***
-***REMOVED***
-
-func getPerms(user string, icon string, mention string) *dsg.MessageEmbed ***REMOVED***
-	a := f.Contains(f.MyBot.Users.AlphaUsers, user)
-	b := f.Contains(f.MyBot.Users.BravoUsers, user)
-	c := f.Contains(f.MyBot.Users.CharlieUsers, user)
-
-	return &dsg.MessageEmbed***REMOVED***
-		Author:      &dsg.MessageEmbedAuthor***REMOVED******REMOVED***,
-		Color:       0x1FD36F,
-		Title:       "User Permission information",
-		Description: "Report for " + mention + "'s permissions under skilbot.",
-		Thumbnail: &dsg.MessageEmbedThumbnail***REMOVED***
-			URL:    icon,
-			Width:  64,
-			Height: 64,
-		***REMOVED***,
-		Fields: []*dsg.MessageEmbedField***REMOVED***
-			&dsg.MessageEmbedField***REMOVED***
-				Name:   "Alpha?",
-				Value:  strconv.FormatBool(a),
-				Inline: true,
-			***REMOVED***,
-			&dsg.MessageEmbedField***REMOVED***
-				Name:   "Bravo Permissions:",
-				Value:  strconv.FormatBool(b),
-				Inline: true,
-			***REMOVED***,
-			&dsg.MessageEmbedField***REMOVED***
-				Name:   "Charlie Permissions:",
-				Value:  strconv.FormatBool(c),
 				Inline: true,
 			***REMOVED***,
 		***REMOVED***,
--- src/foundation/module.go
+++ src/foundation/module.go
@@ -1,9 +1,18 @@
 package foundation
 
 import (
-//dsg "github.com/bwmarrin/discordgo"
+	dsg "github.com/bwmarrin/discordgo"
 )
 
+/* # Big Bot Boye
+* This struct is all about the bot, its prefrences, authentication, so on.
+*
+* The fields are self explanitory so I'm not going to detail them.
+*
+* TODO: I think the auth section needs to be either a different json file or
+*       passed in as parameters because the rest isn't nearly as important.
+*
+ */
 type BotType struct ***REMOVED***
 	Auth struct ***REMOVED***
 		Username string `json:"username"`
@@ -22,14 +31,97 @@ type BotType struct ***REMOVED***
 		BlacklistedChannels []string `json:"blacklistedChannels"`
 	***REMOVED*** `json:"perms"`
 	Users struct ***REMOVED***
-		BlacklistedUsers []string `json:"blacklistedUsers"`
-		AlphaUsers       []string `json:"alphaUsers"`
-		BravoUsers       []string `json:"bravoUsers"`
-		BravoOverride    bool     `json:"bravoOverride"`
-		CharlieUsers     []string `json:"charlieUsers"`
+		RoleWhitelist    bool     `json:"roleWhitelist"`
+		ReportPermFails  bool     `json:"reportPermissionFailures"`
+		BlacklistedRoles []string `json:"blacklistedRoles"`
+		AdminRoles       []string `json:"adminRoles"`
+		StaffRoles       []string `json:"staffRoles"`
+		StandardRoles    []string `json:"standardUser"`
 	***REMOVED*** `json:"users"`
 ***REMOVED***
 
+/* # Get the guild a message was sent in.
+* What a pain in the arse.
+*
+* Parameters:
+* - s (type *discordgo.Session) | The current running discord session,
+*     (discordgo needs that always apparently)
+* - message (type *discordgo.Message) | the author's id is extracted from this.
+*
+* Returns:
+* - st (type *discordgo.Guild) | The guild the message was found in
+* - err (type error)           | If an error was encountered during the process
+*	This error is an SEP (someone else's problem).
+ */
+func GetGuild(s *dsg.Session, m *dsg.Message) (st *dsg.Guild, err error) ***REMOVED***
+	chn, err := s.Channel(m.ChannelID)
+	if err != nil ***REMOVED***
+		return &dsg.Guild***REMOVED******REMOVED***, err
+	***REMOVED***
+
+	gid := chn.GuildID
+
+	return s.Guild(gid)
+
+***REMOVED***
+
+/* # Checks if user can run a command
+* This is a more detailed check to see if a user has the role perms to run a
+* command. It is a very complicated wrapper for what would in theory be a
+* very simple task.
+*
+* Do not confuse this with commandHandler's "canTriggerBot()". This function
+* determines if a user has a specific role, that just checks if the command is
+* run in the correct channel and if they aren't blacklisted.
+*
+* Parameters:
+* - s (type *discordgo.Session) The current running discord session,
+*     (discordgo needs that always apparently)
+* - message (type *discordgo.Message) | the author's id is extracted from this.
+* - role (type string) | the role **ID NUMBER!!** that needs to be matched.
+*
+* Returns:
+* - bool | if the role was found or not, if an error was found it will be false
+* - err  | if an error was encountered during the process.
+*     To be handled by not-the-function
+*
+* User's whose roles are considered "admin" by the json config file return an
+* automatic true regardless if they have the role listed. Hence if you want to
+* lock off a command to only "admin" users, provide an empty string
+ */
+func HasRole(s *dsg.Session, m *dsg.Message, role string) (bool, error) ***REMOVED***
+	guild, err := GetGuild(s, m)
+	if err != nil ***REMOVED***
+		return false, err
+	***REMOVED***
+	member, err := s.GuildMember(guild.ID, m.Author.ID)
+	if err != nil ***REMOVED***
+		return false, err
+	***REMOVED***
+	for _, b := range MyBot.Users.AdminRoles ***REMOVED***
+		if Contains(member.Roles, b) ***REMOVED***
+			return true, nil
+		***REMOVED***
+	***REMOVED***
+
+	hasRole := Contains(member.Roles, role)
+	return hasRole, nil
+***REMOVED***
+
+/* # Check if item is in array
+* This function checks if a value is in a slice (string only)
+*
+* Parameters:
+* - list ([]string) | the slice to be checking against
+* - item (string)   | the item looked for in the slice
+*
+* Returns:
+* - bool | If the item was found or not
+*
+* NOTE: If another Contains() funciton is needed for a different type, rename
+* this function to ContainsSliceString() and the other function to
+* ContainsSlice<T>() where <T> is the generic type.
+ */
 func Contains(list []string, item string) bool ***REMOVED***
 	for _, b := range list ***REMOVED***
 		if b == item ***REMOVED***
@@ -39,4 +131,9 @@ func Contains(list []string, item string) bool ***REMOVED***
 	return false
 ***REMOVED***
 
+// An initialized instance of the BotType for use everywhere in this project.
 var MyBot BotType
+
+// A discordgo session global variable as .Session is needed for a lot.
+// This is written to in the main.go file.
+var DG *dsg.Session
