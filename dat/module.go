c181a48628c7a3be1639bd74ed84cdff14e0a795
Whitman Huntley <whitman.colm@gmail.com>
2018-02-08 14:14:41 -0500

    Added a logger?
    At least started on one. Will actually  test soon.

--- dat/module.go
+++ dat/module.go
@@ -2,11 +2,36 @@ package nil
 
 import (
 	"encoding/json"
+	"flag"
+	dsg "github.com/bwmarrin/discordgo"
 	f "github.com/skilstak/discord-public/lib"
 	"io/ioutil"
+	"log"
+	"os"
+	"time"
 )
 
-func GetBotInfo(path string) (f.BotType, error) {
+var (
+	time   string
+	path   string
+	logger *log.Logger
+)
+
+func init() {
+	flag.StringVar(&path, "p", "./dat", "Path to directory where the bot can store and work with data")
+	flag.Parse()
+
+	time = time.Now().Format("2006-01-02@15:04:05")
+
+	file, err := os.Create(logpath + "logs/system-logs@" + time + ".log")
+	if err != nil {
+		panic(err)
+	}
+
+	logger = log.New(file, "", Ldate|Ltime|Llongfile|LUTC)
+}
+
+func GetBotInfo() (f.BotType, error) {
 	raw, err0 := ioutil.ReadFile(path)
 	var b f.BotType
 
@@ -21,4 +46,14 @@ func GetBotInfo(path string) (f.BotType, error) {
 	}
 
 	return b, nil
+}
+
+func Panic(s *dsg.Session, m *dsg.MessageCreate, err error, fatal bool) {
+	s.ChannelMessageSend(m.ChannelID, "**ERROR ENCOUNTERED. DETAILS FOLLOW:**\n```"+err.Error()+"```\nThis incident will be reported.")
+	if fatal {
+		s.ChannelMessageSend(m.ChannelID, "The bot is now \"gracefully\" force quitting, however it will fail to close out of its session with discord and may still apear online.\n*Have a good day!*")
+		logger.Fatalln(err.Error())
+	} else {
+		logger.Println(err.Error())
+	}
 }
